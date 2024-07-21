7f055b581279aeb99cc5a78cf11a1fa2bb77588c
Morgan <mle150@uclive.ac.nz>
2024-07-21 20:53:26 +1200

    Started board class

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 import numpy as np
 import math
 
+def intro():
+    print("Welcome to the connect game!")
+    print("\n")
+    play = input("Do you want to play? (y/n)")
+    return play.lower() == 'y'
+
+class board:
+    def init_board(self):
+        self.connect_board = np.zeros((6,7))
+    
+    def check_full(self, column):
+        return self.connect_board[0][column] != 0
+    
+
+
+
+
+    
