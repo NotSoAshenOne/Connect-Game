dcda4165a1efd8bdc1d0b1296f409fe5521e976e
Morgan <mle150@uclive.ac.nz>
2024-07-22 09:02:43 +1200

    Did the player turns and main game loop. The connect 4 game is playable

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Connect-Game
+
+This is a simple connect 4 game that you can play in the terminal. You have the option to play on a variety of boards
--- main.py
+++ main.py
@@ -15,22 +15,22 @@ class Board:
         return self.connect_board[0][column] != 0
     
     def place_coin(self, column, player):
-        i = self.rows
+        i = self.rows - 1
         while self.connect_board[i][column] != 0:
             i += -1
         self.connect_board[i][column] = player
         return self.check_win(i, column, player)
 
     def check_vertical(self, row, column, player):
+        connected_up = 0
+        connected_down = 0
         if row != self.rows - 1:
             i = row + 1
-            connected_up = 0
             while i < self.rows and self.connect_board[i][column] == player:
                 connected_up += 1
                 i += 1
         if row != 0:
             i = row - 1
-            connected_down = 0
             while i >= 0 and self.connect_board[i][column] == player:
                 connected_down += 1
                 i += -1
@@ -38,15 +38,15 @@ class Board:
         return connected_vertical
     
     def check_horizontal(self, row, column, player):
+        connected_left = 0
+        connected_right = 0
         if column != 0:
             i = column - 1
-            connected_left = 0
             while i >= 0 and self.connect_board[row][i] == player:
                 connected_left += 1
                 i += -1
         if column != self.columns - 1:
             i = column + 1
-            connected_right = 0
             while i < self.columns and self.connect_board[row][i] == player:
                 connected_right += 1
                 i += 1
@@ -54,10 +54,11 @@ class Board:
         return connected_horizontal
     
     def check_u_diagonal(self, row, column, player):
+        connected_right = 0
+        connected_left = 0
         if column != self.columns - 1 and row != self.rows:
             i = row + 1
             j = column + 1
-            connected_right = 0
             while i < self.rows and j < self.columns and self.connect_board[i][j] == player:
                 connected_right += 1
                 i += 1
@@ -65,7 +66,6 @@ class Board:
         if column != 0 and row != 0:
             i = row - 1
             j = column - 1
-            connected_left = 0
             while i >= 0 and j >= 0 and self.connect_board[i][j] == player:
                 connected_left += 1
                 i += -1
@@ -74,10 +74,11 @@ class Board:
         return connected_u_diagonal
     
     def check_d_diagonal(self, row, column, player):
+        connected_right = 0
+        connected_left = 0
         if column != self.columns - 1 and row != 0:
             i = row - 1
             j = column + 1
-            connected_right = 0
             while i >= self.rows and j < self.columns and self.connect_board[i][j] == player:
                 connected_right += 1
                 i += -1
@@ -85,7 +86,6 @@ class Board:
         if column != 0 and row != self.rows - 1:
             i = row + 1
             j = column - 1
-            connected_left = 0
             while i < self.rows and j >= 0 and self.connect_board[i][j] == player:
                 connected_left += 1
                 i += 1
@@ -107,16 +107,42 @@ def intro():
 
 def game():
     connect_board = Board(6,7)
+    connect_board.show_board()
     win = False
+    player = 1
     while win == False:
-        connect_board.show_board()
-        column = str(input("Player 1, where would you like to place a coin?")) - 1
-        if connect_board.check_full(column) == True:
-            column = str(input("Player 1, where would you like to place a coin?")) - 1
+        if player == 1 and win == False:
+            column = int(input("Player 1, where would you like to place a coin?\n")) - 1
+            # if connect_board.check_full(column) == True:
+            #     column = int(input("This column is full. Please choose another one.\n")) - 1
+            while connect_board.check_full(column) == True:
+                column = int(input("This column is full. Please choose another one.\n")) - 1
+            win = connect_board.place_coin(column, player)
+            print("\n")
+            connect_board.show_board()
+            print("\n")
+            if win == False:
+                player = 2
+        if player == 2 and win == False:
+            column = int(input("Player 2, where would you like to place a coin?\n")) - 1
+            while connect_board.check_full(column) == True:
+                column = int(input("This column is full. Please choose another one.\n")) - 1
+            win = connect_board.place_coin(column, player)
+            print("\n")
+            connect_board.show_board()
+            print("\n")
+            if win == False:
+                player = 1
+    
+    print(f"\nCongratulations Player{player}, you won!\n\n" )
+    connect_board.show_board()
+    
+    
 
-# def main():
-#     while intro() == False:
-#         intro()
-#     game()
+def main():
+    while intro() == False:
+        intro()
+    game()
+    exit()
 
-# main()
+main()
