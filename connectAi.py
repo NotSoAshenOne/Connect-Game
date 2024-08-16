d4b764ccaf02adf2e2cdfc3d53de47160bf46463
Morgan <mle150@uclive.ac.nz>
2024-08-16 12:04:55 +1200

    Implemented the greedy algorithm for the computer

--- connectAi.py
+++ connectAi.py
@@ -1,12 +1,9 @@
 import math
-#from main import Board
+import random
 
 class Computer_Player:
 
-
-
     DEPTH_LIMIT = 6
-    column_dict = {'a':0, 'b':1, 'c':2, 'd':3, 'e':1}
 
     def __init__(self, game_board, difficulty, player):
         self.game_board = game_board
@@ -14,50 +11,28 @@ class Computer_Player:
         self.player = player
         self.columns = [i for i in range(1, game_board.rows+1)]
 
-    def find_next_move(self, game_board):
-        """Use some dynamic progamming algorithm to choose the best move"""
-        if self.difficulty == "easy":
-            self.greedy_move(self.game_board)
-        else:
-            self.computer_play()
-    
-    # def greedy_move(self, game_board):
-    #     """Use greedy algorithm"""
-        
+    def greedy(self, game_board, player):
+        """Finds the next move by using the greedy algorithm"""
+        best_connections = 0
+        best_column = []
 
-
-    # def find_move(self, column, player, depth, score, game_board_copy):
-    #     if depth <= self.DEPTH_LIMIT:
-    #         if game_board_copy.place_coin(column, player) == True:
-    #         #if game_board_copy.check_win()
-    #             if player == self.player:
-    #                 return (-(score+1), column)
-    #             else:
-    #                 return (-(score+1), column)
-    #         else:
-    #             next = min((self.find_move(0, (player+1)%2 + 1, depth +1, score+1, game_board_copy.copy())), (self.find_move(1, (player+1)%2 + 1, depth +1, score+1, game_board_copy.copy())),
-    #                        (self.find_move(2, (player+1)%2 + 1, depth +1, score+1, game_board_copy.copy())), (self.find_move(3, (player+1)%2 + 1, depth +1, score+1, game_board_copy.copy())),
-    #                        (self.find_move(4, (player+1)%2 + 1, depth +1, score+1, game_board_copy.copy())), (self.find_move(5, (player+1)%2 + 1, depth +1, score+1, game_board_copy.copy())),
-    #                        (self.find_move(6, (player+1)%2 + 1, depth +1, score+1, game_board_copy.copy())))
-    #             return (score + next[0], column)
-    #     else:
-    #         return (0, column)
-        
-    # def computer_play(self):
-    #     next_move = min((self.find_move(0, self.player, 0, 0, self.game_board.copy())),(self.find_move(1, self.player, 0, 0, self.game_board.copy())),(self.find_move(2, self.player, 0, 0, self.game_board.copy())),
-    #                     (self.find_move(3, self.player, 0, 0, self.game_board.copy())),(self.find_move(4, self.player, 0, 0, self.game_board.copy())),(self.find_move(5, self.player, 0, 0, self.game_board.copy())),
-    #                     (self.find_move(6, self.player, 0, 0, self.game_board.copy())))
-    #     print(next_move)
-    #     win = self.game_board.place_coin(next_move[1], self.player)
-    #     return win
+        for x in range(0, game_board.columns):
+            row = game_board.next_row(x)
+            connections = max(game_board.check_vertical(row, x, player), game_board.check_horizontal(row, x, player), game_board.check_u_diagonal(row, x, player), game_board.check_d_diagonal(row, x, player))
+            if connections >= best_connections:
+                if connections == best_connections:
+                    best_column.append(x)
+                if connections > best_connections:
+                    best_column = [x]
+        return random.choice(best_column)
 
     def negamax(self, game_board, player, depth):
-
+        """Determines the next move by using the recursive minmax algorithm"""
         if depth < self.DEPTH_LIMIT:
-            if game_board.getNumMoves() == game_board.columns * game_board.rows:
+            if game_board.getNumMoves() == game_board.columns * game_board.rows:    #Checks if the table is filled.
                 return (0, -1)
 
-            for x in range(0,game_board.columns):
+            for x in range(0,game_board.columns):   # Checks if the player can win in the next move.
                 row = game_board.next_row(x)
                 if game_board.check_full(x) == False and game_board.check_win(row, x, player) == True:
                     return ((game_board.columns * game_board.rows) - (game_board.getNumMoves()/2), x)
@@ -65,19 +40,18 @@ class Computer_Player:
             best_score = -game_board.columns+game_board.rows
             best_column = -1
 
-            for x in range(0,game_board.columns):
-                #print(x)
+            for x in range(0,game_board.columns):   # Recursively checks the next possible moves, giving higher scores to those that cause the player to win.
                 if game_board.check_full(x) == False:
                     game_board_copy = game_board.copy()
-                    #print("copy ", x," ", game_board_copy)
                     game_board_copy.place_coin(x, player)
                     score = -((self.negamax(game_board_copy, (player%2)+1, depth+1))[0])
                     if score > best_score:
                         best_score = score
                         best_column = x
-
             return (best_score, best_column)
         else:
+            # In the case that the depth limit is reached, checks if any moves can win otherwise gives a score based on the column
+            # with the middle 3 having a higher score than the outside 4.
             score = 0
             for x in range(0,game_board.columns):
                 row = game_board.next_row(x)
@@ -87,11 +61,13 @@ class Computer_Player:
                     score = game_board.columns
             return (score,0) 
                     
-    
-    def play(self):
-        
+    def play_minmax(self):
         column = self.negamax(self.game_board, self.player, 0)[1]
-        #print(column)
+        win = self.game_board.place_coin(column, self.player)
+        return win
+    
+    def play_greedy(self):
+        column = self.negamax(self.game_board, self.player, 0)[1]
         win = self.game_board.place_coin(column, self.player)
         return win
      
--- main.py
+++ main.py
@@ -1,137 +1,5 @@
 from connectBoard import Board
 from connectAi import Computer_Player as CompPlayer
-
-
-# class Board:
-#     """Class that defines the board.
-#         Contains the functionality for placing coins and checking win conditions"""
-    
-#     def __init__(self, rows=6, columns=7, other_board = 's', numMoves=0,):
-#         self.rows = rows
-#         self.columns = columns
-#         self.numMoves = 0;
-#         if type(other_board) == str:
-#             self.connect_board = np.zeros((self.rows,self.columns))
-#         else:
-#             self.connect_board = other_board
-        
-
-    
-#     def show_board(self):
-#         print(self.connect_board)
-
-    
-#     def check_full(self, column):
-#         """Returns True if the 0 row is not 0 i.e. the column is full"""
-#         return self.connect_board[0][column] != 0
-    
-#     def next_row(self, column):
-#         i = 0
-#         while i < self.rows and self.connect_board[i][column] == 0:
-#             i += 1
-#         return i - 1
-
-    
-#     def place_coin(self, column, player):
-#         # i = self.rows - 1
-#         # while self.connect_board[i][column] != 0:
-#         #     i += -1
-#         # self.connect_board[i-1][column] = player
-#         i = 0
-#         # print(i, column)
-#         # print(self.connect_board)
-#         # print("index", self.connect_board[i, column])
-#         while i < self.rows and self.connect_board[i][column] == 0:
-#             i += 1
-#         self.connect_board[i-1, column] = player
-#         self.setNumMoves()
-#         return self.check_win(i-1, column, player)
-
-#     def check_vertical(self, row, column, player):
-#         connected_up = 0
-#         connected_down = 0
-#         if row != self.rows - 1:
-#             i = row + 1
-#             while i < self.rows and self.connect_board[i][column] == player:
-#                 connected_up += 1
-#                 i += 1
-#         if row != 0:
-#             i = row - 1
-#             while i >= 0 and self.connect_board[i][column] == player:
-#                 connected_down += 1
-#                 i += -1
-#         connected_vertical = 1 + connected_up + connected_down
-#         return connected_vertical
-    
-#     def check_horizontal(self, row, column, player):
-#         connected_left = 0
-#         connected_right = 0
-#         if column != 0:
-#             i = column - 1
-#             while i >= 0 and self.connect_board[row][i] == player:
-#                 connected_left += 1
-#                 i += -1
-#         if column != self.columns - 1:
-#             i = column + 1
-#             while i < self.columns and self.connect_board[row][i] == player:
-#                 connected_right += 1
-#                 i += 1
-#         connected_horizontal = 1 + connected_left + connected_right
-#         return connected_horizontal
-    
-#     def check_u_diagonal(self, row, column, player):
-#         connected_right = 0
-#         connected_left = 0
-#         if column != self.columns - 1 and row != self.rows:
-#             i = row + 1
-#             j = column + 1
-#             while i < self.rows and j < self.columns and self.connect_board[i][j] == player:
-#                 connected_right += 1
-#                 i += 1
-#                 j += 1
-#         if column != 0 and row != 0:
-#             i = row - 1
-#             j = column - 1
-#             while i >= 0 and j >= 0 and self.connect_board[i][j] == player:
-#                 connected_left += 1
-#                 i += -1
-#                 j += -1
-#         connected_u_diagonal = 1 + connected_left + connected_right
-#         return connected_u_diagonal
-    
-#     def check_d_diagonal(self, row, column, player):
-#         connected_right = 0
-#         connected_left = 0
-#         if column != self.columns - 1 and row != 0:
-#             i = row - 1
-#             j = column + 1
-#             while i >= self.rows and j < self.columns and self.connect_board[i][j] == player:
-#                 connected_right += 1
-#                 i += -1
-#                 j += 1
-#         if column != 0 and row != self.rows - 1:
-#             i = row + 1
-#             j = column - 1
-#             while i < self.rows and j >= 0 and self.connect_board[i][j] == player:
-#                 connected_left += 1
-#                 i += 1
-#                 j += -1
-#         connected_d_diagonal = 1 + connected_left + connected_right
-#         return connected_d_diagonal
-
-#     def check_win(self, row, column, player):
-#         if self.check_d_diagonal(row, column, player) >= 4 or self.check_u_diagonal(row, column, player) >= 4 or self.check_horizontal(row, column, player) >= 4 or self.check_vertical(row, column, player) >= 4:
-#             return True
-#         else:
-#             return False
-    
-#     def copy(self):
-#         return Board(self.rows, self.columns, np.copy(self.connect_board), self.numMoves)
-    
-#     def getNumMoves(self):
-#         return self.numMoves
-#     def setNumMoves(self):
-#         self.numMoves += 1
     
 def intro():
     print("\nWelcome to the connect game!\n")
@@ -182,20 +50,15 @@ def game(ai):
                 if win == False:
                     player = 2
             if player == 2 and win == False:
-                win = computer.play()
+                win = computer.play_minmax()
                 print("\n")
                 connect_board.show_board()
                 print("\n")
                 if win == False:
                     player = 1
             
-                
-       
-
-    
     print(f"\nCongratulations Player{player}, you won!\n\n" )
     connect_board.show_board()
-    
     
 
 def main():
